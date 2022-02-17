11c43a58e9f704badf2ea7dec8422c96d8b0cff2
danie <danie@LAPTOP-PG0D63MN.lan>
2022-02-17 13:02:12 +0100

    Prova

---
+++ Prova2/server/src/main/java/it/Sofia_Dani/Prova2/ServerApplication.java
@@ -0,0 +1,14 @@
+package it.Sofia_Dani.Prova2;
+
+import com.canoo.platform.remoting.server.spring.DolphinPlatformRemotingApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@DolphinPlatformRemotingApplication
+public class ServerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(new Class[]{ServerApplication.class}, args);
+    }
+}
+
